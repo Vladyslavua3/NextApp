b58b96ae8383badcf5d38addeed641ded3c008a7
Vladyslavua <vladgershman@gmail.com>
2023-05-02 13:59:35 +0200

    init:hook useLoader with NProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,28 +2,31 @@ import type {AppProps} from 'next/app';
 import {ReactElement, ReactNode, useState} from 'react';
 import {NextPage} from 'next';
 import {QueryClient} from "@tanstack/query-core";
-import {QueryClientProvider,Hydrate} from "@tanstack/react-query";
+import {QueryClientProvider, Hydrate} from "@tanstack/react-query";
+import {useLoader} from "../assets/hooks/useLoader";
+import '../styles/nprogress.css'
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
-  getLayout?: (page: ReactElement) => ReactNode;
+    getLayout?: (page: ReactElement) => ReactNode;
 };
 
 type AppPropsWithLayout = AppProps & {
-  Component: NextPageWithLayout;
+    Component: NextPageWithLayout;
 };
 
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
+export default function App({Component, pageProps}: AppPropsWithLayout) {
 
-  const [queryClient] = useState(() => new QueryClient())
+    const [queryClient] = useState(() => new QueryClient())
 
+    useLoader()
 
-  const getLayout = Component.getLayout ?? ((page) => page);
+    const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydrateState}>
-          <Component {...pageProps} />
-        </Hydrate>
-      </QueryClientProvider>
-  );
+    return getLayout(
+        <QueryClientProvider client={queryClient}>
+            <Hydrate state={pageProps.dehydrateState}>
+                <Component {...pageProps} />
+            </Hydrate>
+        </QueryClientProvider>
+    );
 }
