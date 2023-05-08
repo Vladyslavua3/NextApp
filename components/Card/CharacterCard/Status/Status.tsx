c923192b4f1b2f8d3e037a0f9eb303655cee8bef
Vladyslavua <vladgershman@gmail.com>
2023-05-08 16:32:06 +0200

    init:Working with image in NEXT

---
+++ components/Card/CharacterCard/Status/Status.tsx
@@ -0,0 +1,19 @@
+import Image, {StaticImageData} from "next/image";
+import {CharacterStatusType} from "assets/api/rick-and-morty-api";
+
+type PropsType = {
+    status: CharacterStatusType
+    src: StaticImageData
+}
+
+
+export const Status = (props: PropsType) => {
+
+    const {status,src} = props
+
+    return (
+        <>
+            <Image src={src} alt={''}/>
+        </>
+    )
+}
