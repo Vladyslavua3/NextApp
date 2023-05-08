7d3af83faa2a46d2165f79cf82527dbd094708e3
Vladyslavua <vladgershman@gmail.com>
2023-05-08 15:19:18 +0200

    fix: fixed imports

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,6 +1,6 @@
 import {NextPage} from "next";
 import {PropsWithChildren} from "react";
-import {Header} from "../../components/Header/Header";
+import {Header} from "components/Header/Header";
 import styled from "styled-components";
 
 export const Layout:NextPage<PropsWithChildren> = (props) => {
--- pages/locations/index.tsx
+++ pages/locations/index.tsx
@@ -1,9 +1,9 @@
-import {PageWrapper} from "../../components/PageWrapper/PageWrapper";
-import {LocationType, ResponseType} from "../../assets/api/rick-and-morty-api";
+import {PageWrapper} from "components/PageWrapper/PageWrapper";
+import {LocationType, ResponseType} from "assets/api/rick-and-morty-api";
 import {dehydrate, useQuery} from "@tanstack/react-query";
 import {QueryClient} from "@tanstack/query-core";
-import {Card} from "../../components/Card/Card";
-import {getLayout} from "../../components/Layout/BaseLayout/BaseLayout";
+import {Card} from "components/Card/Card";
+import {getLayout} from "components/Layout/BaseLayout/BaseLayout";
 
 
 const getLocations = () => {
